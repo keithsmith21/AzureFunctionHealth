738f0e2378373c80305e999419f9c48c2532a4db
Keith Smith <keithsmith@capaxglobal.com>
2020-09-15 16:59:00 -0400

    Initial Commit

---
+++ src/FunctionHealthCheck/HealthChecksBuilder.cs
@@ -0,0 +1,34 @@
+﻿//Copyright(c) .NET Foundation and Contributors
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionHealthCheck
+{
+    internal class HealthChecksBuilder : IHealthChecksBuilder
+    {
+        public HealthChecksBuilder(IServiceCollection services)
+        {
+            Services = services;
+        }
+
+        public IServiceCollection Services { get; }
+
+        public IHealthChecksBuilder Add(HealthCheckRegistration registration)
+        {
+            if (registration == null)
+            {
+                throw new ArgumentNullException(nameof(registration));
+            }
+
+            Services.Configure<HealthCheckServiceOptions>(options =>
+            {
+                options.Registrations.Add(registration);
+            });
+
+            return this;
+        }
+    }
+}
